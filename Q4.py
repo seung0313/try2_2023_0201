5d1c3d15c67edb4b0847677dec6bfa86d679efe6
seung0313 <97012073+seung0313@users.noreply.github.com>
2023-02-02 16:44:02 +0900

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,22 @@
+# Q4 Answer Template
+data = input('숫자로 이루어진 문자열을 입력하세요. ')
+answer = 0
+
+for i in range(len(data)):
+    num = int(data[i])
+    
+    if i == 0:
+        print(f'{num}', end='')
+        answer += num
+    elif answer == 1 or num == 1 or answer == 0 or num == 0:
+        answer += num
+        print(f' + {num}', end='')
+    else:
+        answer *= num
+        print(f' * {num}', end='')
+'''
+    else :
+        answer += num
+        print(f' + {num}', end='')
+'''
+print(f' = {answer}')
